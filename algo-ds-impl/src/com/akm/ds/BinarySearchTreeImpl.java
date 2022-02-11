7f5bdb4b0bee6ac65bbd0f6c1e5bb58441797431
amiyakumarm <88655714+amiyakumarm@users.noreply.github.com>
2022-02-11 20:23:18 +0530

    BST

--- algo-ds-impl/src/com/akm/ds/BinarySearchTreeImpl.java
+++ algo-ds-impl/src/com/akm/ds/BinarySearchTreeImpl.java
@@ -3,3 +3,13 @@ package com.akm.ds;
 public class BinarySearchTreeImpl {
 
 }
+
+class Node {
+	public Node left;
+	public Node right;
+	public int data;
+
+	Node(int data) {
+		this.data = data;
+	}
+}
